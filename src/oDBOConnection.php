95f3013b1a78709c17e12038fcd1b07a7c27d70e
Nate Obray <nate@maloufsleep.com>
2018-01-30 16:09:20 -0700

    updates for namespaces

--- src/oDBOConnection.php
+++ src/oDBOConnection.php
@@ -19,79 +19,95 @@ Class oDBOConnection {
     private $conn;
     private $is_connected = FALSE;
 
-    public function __construct($host, $username, $password, $db_name, $port='3306', $db_engine='innoDB', $char_set="utf8"){
-            $this->host = $host;
-            $this->username = $username;
-            $this->password = $password;
-            $this->db_name = $db_name;
-            $this->port = $port;
-            $this->db_engine = $db_engine;
-            $this->db_char_set = $char_set;
+    public function __construct($host, $username, $password, $db_name, $port='3306', $db_engine='innoDB', $char_set="utf8")
+    {
+        $this->host = $host;
+        $this->username = $username;
+        $this->password = $password;
+        $this->db_name = $db_name;
+        $this->port = $port;
+        $this->db_engine = $db_engine;
+        $this->db_char_set = $char_set;
     }
 
-    public function setUsername( string $username ){
-            $this->username = $username;
+    public function setUsername( string $username )
+    {
+        $this->username = $username;
     }
 
-    public function setPassword( string $password ){
-            $this->password = $password;
+    public function setPassword( string $password )
+    {
+        $this->password = $password;
     }
 
-    public function setHost( $host ){
-            $this->host = $host;
+    public function setHost( $host )
+    {
+        $this->host = $host;
     }
 
-    public function setPort( $port ){
-            $this->port = $port;
+    public function setPort( $port )
+    {
+        $this->port = $port;
     }
 
-    public function setDBName( $name ){
-            $this->db_name = $name;
+    public function setDBName( $name )
+    {
+        $this->db_name = $name;
     }
 
-    public function getDBName(){
-            return $this->db_name;
+    public function getDBName()
+    {
+        return $this->db_name;
     }
 
-    public function getDBEngine(){
-            return $this->db_engine;
+    public function getDBEngine()
+    {
+        return $this->db_engine;
     }
 
-    public function getDBCharSet(){
-            return $this->db_char_set;
+    public function getDBCharSet()
+    {
+        return $this->db_char_set;
     }
 
-    public function connect( $reconnect=FALSE ){
-            
-            global $conn;
-            
-            if( !isSet( $conn ) || $reconnect ){
-                    try {
-                            $conn = new \PDO(
-                                    'mysql:host='.$this->host.';dbname='.$this->db_name.';charset=utf8',
-                                    $this->username,
-                                    $this->password,
-                                    array(
-                                            \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
-                                    ));
-                            $conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
-                            $this->is_connected = TRUE;
-                    } catch(\PDOException $e) {
-                            echo 'ERROR: ' . $e->getMessage(); 
-                            exit();
-                    }
+    public function connect( $reconnect=FALSE )
+    {
+        $this->conn;
+        if( !isSet( $this->conn ) || $reconnect ){
+            try {
+                $this->conn = new \PDO(
+                        'mysql:host='.$this->host.';dbname='.$this->db_name.';charset=utf8',
+                        $this->username,
+                        $this->password,
+                        array(
+                                \PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
+                        ));
+                $this->conn->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $this->is_connected = TRUE;
+            } catch(\PDOException $e) {
+                echo 'ERROR: ' . $e->getMessage(); 
+                exit();
             }
-
-            return $conn;
-
+        }
+        return $this->conn;
     }
 
-    public function getConnection(){
-            return $this->conn;
+    public function getConnection()
+    {
+        return $this->conn;
     }
 
-    public function isConnected(){
-            return $this->is_connected;
+    public function isConnected()
+    {
+        return $this->is_connected;
+    }
+
+    public function __call($name, $arguments=array())
+    {
+        $conn = $this->connect();
+        if( method_exists($conn,$name) ){
+            return $conn->$name(...$arguments);
+        }
     }
 
 }
