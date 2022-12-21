6be604554c99fc6f5cb30c1863c35bfe96be37a2
Nate Obray <nathanobray@gmail.com>
2022-12-21 02:10:17 +0000

    fixed issue with DateTimeCreated default value

--- src/types/DateTimeCreated.php
+++ src/types/DateTimeCreated.php
@@ -8,4 +8,13 @@ class DateTimeCreated extends DateTime
     const UNSIGNED = false;
     const NULLABLE = false;
     const DEFAULT = 'CURRENT_TIMESTAMP';   
+
+    public function __construct(mixed $value=self::DEFAULT)
+    {
+        if($value === 'CURRENT_TIMESTAMP' || $value === null){
+            $this->value = new \DateTime();
+        }  else {
+            $this->value = new \DateTime($value);
+        }
+    }
 }
